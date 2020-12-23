9394abee2695bd9d93e84f4fdf0d2eeccb3d1d33
Mert Okumuş <merto-dvp@hotmail.com>
2020-12-23 22:55:01 +0300

    Initial commit

--- testapp/src/app/app-routing.module.ts
+++ testapp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', loadChildren: () => import('../libs/hw1/hw1.module').then(m => m.Hw1Module) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- testapp/src/app/app.component.spec.ts
+++ testapp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testapp'`, () => {
+  it(`should have as title 'homework1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testapp');
+    expect(app.title).toEqual('homework1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('testapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('homework1 app is running!');
   });
 });
